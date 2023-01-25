2312775fa1e45efc45c8133be6164b9fb731548d
Russell Gordon <rgordon@lcs.on.ca>
2023-01-25 11:18:34 -0500

    Initial Commit

---
+++ StructuresRecap/StructuresRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructuresRecapApp.swift
+//  StructuresRecap
+//
+//  Created by Russell Gordon on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct StructuresRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
